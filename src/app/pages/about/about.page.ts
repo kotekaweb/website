f5fcabe6c131ba58fccc30c62cb457c95cf90cea
kotekaweb <kotekaweb@gmail.com>
2024-02-14 03:02:57 +0900

    Update Web Content
    - Update Home page content
    - Update About page content
    - Update Footer content
    - Update Footer social media content

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './about.page.scss'
 })
 export class AboutPage {
+  history = [
+    {
+      date: 'August 1st, 2017',
+      title: 'Meet Up',
+      detail: 'Just a 3 ordinary people with IT background meeting each other'
+    },
+    {
+      date: 'October 16th, 2017',
+      title: 'First Freelance Project',
+      detail: 'Just a 3 ordinary people with IT background meeting each other'
+    },
+  ]
 
+  team = [
+    {
+      avatar: 'https://picsum.photos/200',
+      name: 'Marshal D. Laurens',
+      position: 'CEO',
+      social: [
+        { icon: 'linkedin', link: '#' },
+        { icon: 'facebook', link: '#' },
+        { icon: 'instagram', link: '#' }
+      ]
+    }
+  ]
 }
